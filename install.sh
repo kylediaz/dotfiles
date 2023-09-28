d67c0a65fa4f2837b1c2cf21ea096d0cb139268d
Kyle Diaz <kyle@kylediaz.com>
2023-09-28 00:46:20 -0400

    Fix install script

--- install.sh
+++ install.sh
@@ -14,7 +14,13 @@ if [ $1 == '--dotfiles' ]; then
   done
 
   # Install packages
-  sudo dnf install i3-gaps nvim rofi alacritty rofi ranger neofetch code
+  sudo dnf install i3-gaps neovim rofi alacritty rofi ranger neofetch 
+
+  # Install VSCode
+  sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+  sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
+  dnf check-update
+  sudo dnf install code
 elif [ $1 == '--docker']; then
   curl https://get.docker.com | sh
   sudo systemctl --now enable docker
